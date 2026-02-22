12596610460332e38341941ea304bfbece1f4b93
nguyennd <nguyennd92.dev@gmail.com>
2026-02-22 21:29:25 +0700

    init project

---
+++ src/main/java/vn/io/nguyen32/crm/CrmApplication.java
@@ -0,0 +1,13 @@
+package vn.io.nguyen32.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmApplication.class, args);
+	}
+
+}
