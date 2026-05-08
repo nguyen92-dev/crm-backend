252ee8ac481261d42feaeba9df82fbab04f987d3
nguyen92-dev <nguyennd92.dev@gmail.com>
2026-05-08 21:00:15 +0700

    size entity

--- src/main/java/vn/io/nguyen32/crm/product/entity/Size.java
+++ src/main/java/vn/io/nguyen32/crm/product/entity/Size.java
@@ -1,6 +1,7 @@
 package vn.io.nguyen32.crm.product.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -9,16 +10,55 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import top.nguyennd.restsqlbackend.abstraction.entity.AbstractEntity;
 
-@EqualsAndHashCode(callSuper = true)
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode
 @Data
 @Entity
 @Table(name = "size")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @SuperBuilder
 @NoArgsConstructor
-public class Size extends AbstractEntity {
+public class Size {
+
+  @EmbeddedId
+  SizeKey id;
+
   @Column(name = "category_size")
   String categorySize;
+
+  @Column(name = "category_id")
+  Integer categoryId;
+
+  @Column(name = "price")
+  Double price;
+
+  @CreatedDate
+  @Column(
+      nullable = false,
+      updatable = false
+  )
+  private LocalDateTime createdAt;
+  @CreatedBy
+  @Column(
+      nullable = false,
+      updatable = false
+  )
+  private String createdBy;
+  @LastModifiedDate
+  @Column(
+      nullable = false
+  )
+  private LocalDateTime updatedAt;
+  @Column(
+      nullable = false
+  )
+  @LastModifiedBy
+  private String updatedBy;
 }
